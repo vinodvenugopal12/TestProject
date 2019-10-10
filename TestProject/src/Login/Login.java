9c3dde6153505d10c2dafcb551c3dbe7e9647f54
vinod-13 <vinod@DESKTOP-0LOOP8T>
2019-10-10 22:43:38 +0530

    Login Package added and enter a loop

---
+++ TestProject/src/Login/Login.java
@@ -0,0 +1,15 @@
+package Login;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.print(i+"vinod-13");
+		}
+	}
+
+}
