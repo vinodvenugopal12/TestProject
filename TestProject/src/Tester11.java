c443c29f7e98aa8157c8a4eb28f6746b61973db8
vinod-test <vinod@DESKTOP-0LOOP8T>
2019-10-10 06:36:30 +0530

    Add new Tester11.java and changes in Test1.java

---
+++ TestProject/src/Tester11.java
@@ -0,0 +1,10 @@
+
+public class Tester11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test2");
+		System.out.println("Tester11 Code");
+	}
+
+}
