eb7fd2a5fdd2c232a4160d48a237a337b4b6c7cc
vinod-hot <vinod@DESKTOP-0LOOP8T>
2019-10-11 06:48:51 +0530

    Added some more comments in Logout

--- TestProject/src/LogOut/LogOut.java
+++ TestProject/src/LogOut/LogOut.java
@@ -5,6 +5,7 @@ public class LogOut {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("vinod-hot-logout");
+System.out.println("vinod-hot-More ADD logout");
 	}
 
 }
