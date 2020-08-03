1b49724aacba4eda089bed511a83e66fb4be298b
vinod <vinod@LAPTOP-7TRP9PA8>
2020-08-03 10:03:05 +0530

    only staged

--- TestProject/src/Tester22.java
+++ TestProject/src/Tester22.java
@@ -4,7 +4,7 @@ public class Tester22 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test2");
-		System.out.println("Tester22 Code");
+		System.out.println("Tester22 Codess");
 	}
 
 }
